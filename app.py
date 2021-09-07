d758655970e5185cb79d377c6c7f584ff5dc5776
AndrewDarwin <djdarz@gmail.com>
2021-09-07 19:18:42 +0000

    Bind data to the edit recipe form

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ def add_recipe():
 
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
-    recipe = mongo.db.tasks.find_one({"_id": ObjectId(recipe_id)})
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_recipe.html", recipe=recipe, categories=categories)
