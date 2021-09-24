bff9e716e2f598c6cd76f6628c3104e2de5fd359
AndrewDarwin <djdarz@gmail.com>
2021-09-24 12:45:10 +0000

    cleaned up app.py code

--- app.py
+++ app.py
@@ -101,7 +101,8 @@ def profile(username):
     user_recipes = mongo.db.recipes.find({"created_by": username})
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username, user_recipes=user_recipes)
+    return render_template/
+    ("profile.html", username=username, user_recipes=user_recipes)
 
     return redirect(url_for("login"))
 
@@ -155,7 +156,8 @@ def edit_recipe(recipe_id):
     
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
-    return render_template("edit_recipe.html", recipe=recipe, categories=categories)
+    return render_template/
+    ("edit_recipe.html", recipe=recipe, categories=categories)
 
 
 @app.route("/delete_recipe/<recipe_id>")
